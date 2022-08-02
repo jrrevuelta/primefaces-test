f09d9beb0d5d4e124333de72a0cc85336d4c6b5f
José Ramón Revuelta <jrr@jrrevuelta.org>
2022-08-02 19:45:51 +0200

    JRR: Added the original files with as little change as possible

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,27 +1,28 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.math.BigDecimal;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
-import lombok.Data;
-
-@Data
 @Named
 @ViewScoped
 public class TestView implements Serializable {
     
+    private static final long serialVersionUID = 1L;
     private String string;
-    private Integer integer;
-    private BigDecimal decimal;
-    private LocalDateTime localDateTime;
     
     @PostConstruct  
     public void init() {
         string = "Welcome to PrimeFaces!!!";
     }
+    
+    public String getString() {
+		return string;
+	}
+    public void setString(String string) {
+		this.string = string;
+	}
 
 }
